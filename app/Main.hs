a10894be483cb3fb53a88612c56d31a53c0e1536
Natxo <natxo.cabre@gmail.com>
2016-09-09 08:42:07 +0200

    added lucid template library

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,9 @@ import Data.Monoid
 import Data.IORef
 import qualified Data.Text as T
 
+import Lucid
+import Data.Text.Lazy (toStrict)
+
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
@@ -20,9 +23,21 @@ main =
 
 app :: SpockM () MySession MyAppState ()
 app =
-    do get root $
-           text "Hello World!"
+    do get root helloSpock -- $ text "Hello World!"
        get ("hello" <//> var) $ \name ->
            do (DummyAppState ref) <- getState
               visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
               text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+
+-- helloSpock :: SpockAction database session state ()
+-- helloSpock = html "Hello, <em>Spock!</em>"
+
+helloSpockHTML :: Html ()
+helloSpockHTML = html_ (do head_  (title_ "Hello!")
+                           body_ (p_ "Hello Spock!!!"))
+
+helloSpock :: SpockAction database session state ()
+helloSpock = lucid helloSpockHTML
+
+lucid :: Html () -> SpockAction database session state ()
+lucid document = html (toStrict (renderText document))
