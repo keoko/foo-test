d7d98833dc22fce22ad13f2525d92122eaf90a7e
Natxo <natxo.cabre@gmail.com>
2016-10-26 08:18:53 +0200

    hello world with servant

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import Web.Spock
-import Web.Spock.Config
+import Data.Proxy
+import Data.Text
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment
 
-import Control.Monad.Trans
-import Data.Monoid
-import Data.IORef
-import qualified Data.Text as T
+type API = Get '[JSON] Text
 
-import Lucid
-import Data.Text.Lazy (toStrict)
+apiHandler :: Server API
+apiHandler = return "hello world"
 
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+proxy :: Proxy API
+proxy = Proxy
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
-
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root helloSpock -- $ text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
-
--- helloSpock :: SpockAction database session state ()
--- helloSpock = html "Hello, <em>Spock!</em>"
-
-helloSpockHTML :: Html ()
-helloSpockHTML = html_ (do head_  (title_ "Hello!")
-                           body_ (p_ "Hello Spock!!!"))
-
-helloSpock :: SpockAction database session state ()
-helloSpock = lucid helloSpockHTML
-
-lucid :: Html () -> SpockAction database session state ()
-lucid document = html (toStrict (renderText document))
+main = run 8080 $ serve proxy apiHandler
