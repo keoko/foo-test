392146fea37cf3662bdd7777b49a2f12c5b29398
Natxo <natxo.cabre@gmail.com>
2016-10-27 00:02:33 +0200

    blaze handler

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,24 @@ import Data.Proxy
 import Data.Text
 import Network.Wai.Handler.Warp
 import Servant
+import Servant.HTML.Blaze
+import Text.Blaze.Html5 (p, Html)
+
 
 type API = Get '[JSON] Text
-  :<|> "code" :> Raw
+  :<|> "create" :> Get '[HTML] Html
+  :<|> Raw
 --type API = "code" :> Raw
 
 apiHandler :: Server API
 apiHandler = return "hello world!!!"
-  :<|> serveDirectory "tutorial"
+  :<|> createPageHandler
+  :<|> serveDirectory "web"
 --apiHandler = serveDirectory "tutorial"
+
+createPageHandler = return page
+  where page :: Html -- the Html from blaze
+        page = p "hello"
 
 proxy :: Proxy API
 proxy = Proxy
