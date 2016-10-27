8288e1793a7c6115345a3c30cc87ba6375b8ef96
Natxo <natxo.cabre@gmail.com>
2016-10-27 09:46:45 +0200

    faked create interview post method

--- app/Main.hs
+++ app/Main.hs
@@ -14,16 +14,22 @@ import Text.Blaze.Html5 (p, Html)
 
 type API = Get '[JSON] Text
   :<|> "create" :> Get '[HTML] Html
+  :<|> "create" :> Post '[HTML] Html
   :<|> Raw
 
 apiHandler :: Server API
 apiHandler = return "hello world!!!"
   :<|> createPageHandler
+  :<|> createPostHandler
   :<|> serveDirectory "web"
 
 createPageHandler = return page
   where page :: Html
         page = p "hello"
+
+createPostHandler = return page
+  where page :: Html
+        page = p "create post handler"
 
 proxy :: Proxy API
 proxy = Proxy
