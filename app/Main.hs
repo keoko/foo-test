29ab1057d6f49b70f52a7f83512cc9ae8aaedc71
Natxo <natxo.cabre@gmail.com>
2016-10-28 19:01:08 +0200

    moved app logic to Lib file

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-
 module Main where
 
-import Data.Proxy
-import Data.Text
-import Network.Wai.Handler.Warp
-import Servant
-import Servant.HTML.Blaze
-import Text.Blaze.Html5 (p, Html)
-
-
-type API = Get '[JSON] Text
-  :<|> "create" :> Get '[HTML] Html
-  :<|> "create" :> Post '[HTML] Html
-  :<|> Raw
-
-apiHandler :: Server API
-apiHandler = return "hello world!!!"
-  :<|> createPageHandler
-  :<|> createPostHandler
-  :<|> serveDirectory "web"
-
-createPageHandler = return page
-  where page :: Html
-        page = p "hello"
-
-createPostHandler = return page
-  where page :: Html
-        page = p "create post handler"
-
-proxy :: Proxy API
-proxy = Proxy
+import Lib
 
 main :: IO ()
-main = run 8080 $ serve proxy apiHandler
+main = do
+  let port = 8080
+  putStrLn $ "Running in http://localhost:" ++ show port ++ "/"
+  startApp port
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( startApp
+  ) where
+
+import Data.Proxy
+import Data.Text
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Blaze
+import Text.Blaze.Html5 (p, Html)
+
+
+type API = Get '[JSON] Text
+  :<|> "create" :> Get '[HTML] Html
+  :<|> "create" :> Post '[HTML] Html
+  :<|> Raw
+
+apiHandler :: Server API
+apiHandler = return "hello world!!!"
+  :<|> createPageHandler
+  :<|> createPostHandler
+  :<|> serveDirectory "web"
+
+createPageHandler = return page
+  where page :: Html
+        page = p "hello"
+
+createPostHandler = return page
+  where page :: Html
+        page = p "create post handler"
+
+proxy :: Proxy API
+proxy = Proxy
+
+startApp :: Int -> IO ()
+startApp port = run port $ serve proxy apiHandler
