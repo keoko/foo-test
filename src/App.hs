b2b8095c94a0e35e0f87c1a4e11ab99b0e8c840b
Natxo <natxo.cabre@gmail.com>
2016-11-09 08:32:10 +0100

    removed unused imports in App

--- src/App.hs
+++ src/App.hs
@@ -12,29 +12,12 @@ module App
 
 import Api (Api, api)
 import Models
-import Data.Proxy
-import Data.Text as T (Text, unpack, pack)
-import Data.ByteString as BS (pack, unpack, ByteString)
--- import Data.ByteString.Lazy as BS (ByteString, readFile, pack, unpack)
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Servant
-import Servant.HTML.Blaze
-import Text.Blaze.Html5 (p, Html, toHtml)
-import Control.Applicative
-import Data.Aeson
-import Data.Monoid
-import Control.Monad.Trans.Either
 import Control.Monad.IO.Class
-import Text.Regex.PCRE
-import Data.List (sortOn)
-import Database.Persist
-import Database.Persist.Sql
-import Data.Aeson
 import System.Random
 import System.IO.Unsafe
-
-import Database.Persist.TH
 import Database.Persist.Sqlite
 
 
