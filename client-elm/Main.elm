07fef2c5928352cb08a10fb42c6c688ad2508a7e
Natxo <natxo.cabre@gmail.com>
2016-11-16 08:18:09 +0100

    navigation example

--- client-elm/Main.elm
+++ client-elm/Main.elm
@@ -2,12 +2,79 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-
-
-view model =
-    div []
-        [ h1 [] [ text model ] ]
+import Navigation
 
 
 main =
-    view "hello world"
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { history : List Navigation.Location
+    }
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( Model [ location ]
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UrlChange Navigation.Location
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange location ->
+            ( { model | history = location :: model.history }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    div []
+        [ h1 [] [ text "Pages" ]
+        , ul [] (List.map viewLink [ "bears", "cats", "dogs", "elephants", "fish" ])
+        , h1 [] [ text "History" ]
+        , ul [] (List.map viewLocation model.history)
+        ]
+
+
+viewLink : String -> Html msg
+viewLink name =
+    li [] [ a [ href ("#" ++ name) ] [ text name ] ]
+
+
+viewLocation : Navigation.Location -> Html msg
+viewLocation location =
+    li [] [ text (location.pathname ++ location.hash) ]
